cec27f9a5c8ec72aafa5f746c681ec48ab674c32
glabular <glu.max06@gmail.com>
2025-07-07 16:50:23 +0200

    ScannedFileEntry docs added.

--- MultiCopierWPF/Models/ScannedFileEntry.cs
+++ MultiCopierWPF/Models/ScannedFileEntry.cs
@@ -1,7 +1,18 @@
 ﻿namespace MultiCopierWPF.Models;
 
+/// <summary>
+/// Represents basic metadata for a file discovered during a directory scan.
+/// </summary>
 public class ScannedFileEntry
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScannedFileEntry"/> class, representing a file entry with its
+    /// associated metadata.
+    /// </summary>
+    /// <param name="fullPath">The full path of the file, including the directory and file name.</param>
+    /// <param name="fileName">The name of the file, including its extension.</param>
+    /// <param name="fileSize">The size of the file in bytes.</param>
+    /// <param name="lastModified">The date and time when the file was last modified.</param>
     public ScannedFileEntry(string fullPath, string fileName, long fileSize, DateTime lastModified)
     {
         FullPath = fullPath;
@@ -10,11 +21,23 @@ public class ScannedFileEntry
         LastModified = lastModified;
     }
 
+    /// <summary>
+    /// Gets or sets the full path to the file or directory.
+    /// </summary>
     public string FullPath { get; set; }
 
+    /// <summary>
+    /// Gets or sets the name of the file.
+    /// </summary>
     public string FileName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the size of the file in bytes.
+    /// </summary>
     public long FileSize { get; set; }
 
+    /// <summary>
+    /// Gets or sets the date and time when the object was last modified.
+    /// </summary>
     public DateTime LastModified { get; set; }
 }
